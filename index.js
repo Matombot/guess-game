50dcbe70e11281d1f49e934c4d61a611fb582d40
tshifhiwa <matombot@gmail.com>
2022-02-24 11:35:29 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,36 +1,33 @@
 let randomNumber = Math.ceil((Math.random() * 100))
 
-const number_guess= document.querySelector(".number_guess");
+const number_guess = document.querySelector(".number_guess");
 const guess_btn = document.querySelector(".guess_btn");
 const message = document.querySelector(".message");
 
-function guessBtnClicked(){
-const guessedNumber = Number(number_guess.value);
+function guessBtnClicked() {
+    const guessedNumber = Number(number_guess.value);
 
-if (guessedNumber > 100 || guessedNumber <= 0){
-    message.innerHTML = "Error , out of range" ;
-    // alert ("error , out of range")
-}
- else if  (guessedNumber < randomNumber) {
+    if (guessedNumber > 100 || guessedNumber <= 0) {
+        message.innerHTML = "Error , out of range";
+        // alert ("error , out of range")
+    }
+    else if (guessedNumber < randomNumber) {
 
-    message.innerHTML ="Your guess is too low" ;
-}
-else if (guessedNumber > randomNumber) {
-    message.innerHTML ="Your guess is too high" ;
+        message.innerHTML = "Your guess is too low";
+    }
+    else if (guessedNumber > randomNumber) {
+        message.innerHTML = "Your guess is too high";
+    }
+
+    else {
+        message.innerHTML = `Correct , the secret number is ${guessedNumber}`
+        setTimeout(function () {
+            randomNumber = Math.ceil((Math.random() * 100))  
+            message.innerHTML = "New game started !";
+        }, 5000);
+
+    }
 }
 
-else{
-    message.innerHTML =`Correct , the secret number is ${guessedNumber}`
-}
-//setTimeout (function (){
-//   message.innerHTML = "" ;
-//}, 5000);
-}
 
-setTimeout (function refresh(){
-    message.innerHTML = "" ;
-}, 5000);
 guess_btn.addEventListener('click', guessBtnClicked)
-setTimeout(function guessBtnClicked(){
-    message.innerHTML = "New game started !";
-}, 3000);
